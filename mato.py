f2d5f0e0247f7d58d8c6acfdbfa43e6d6a6702cd
MatheoPf <matheo.pfranger@gmail.com>
2025-11-06 09:30:39 +0100

    change columns name

--- mato.py
+++ mato.py
@@ -14,11 +14,16 @@ data = data.drop(columns=data.columns[:3])
 data = data.drop(columns=data.columns[20:])
 
 def data_clean(data):
+    # Renommage des colonnes
+    new_cols = ["musique intrumentale ou avec parole", "support écouté", "station radio", "genre musicale", "période musicale", "langue écouté", "type de parole", "frequence ecouté artiste émergent", "tempo", "fréquence en travaillant", "fréquence en sport", "fréquence en cuisine", "fréquence en transport", "fréquence en passant le temps", "fréquence écoute mensuel", "fréquence écoute journalier", "genre", "age", "environnement", "situation professionnel"]
+    data.columns = new_cols
     text_cols = data.select_dtypes(include=['object']).columns
+    
     for col in text_cols:
         # Remplace "mot / mot" par "mot;mot" dans toutes les colonnes texte
         data[col] = data[col].str.replace(r'\s*/\s*', ';', regex=True)
-        data[col] = data[col].str.strip()
+        data[col] = data[col].str.replace(r'\s*,\s*', ',', regex=True)
+        data[col] = data[col].str.strip() 
         
     data.to_csv('cleaned_data.csv', index=False)
     return data
